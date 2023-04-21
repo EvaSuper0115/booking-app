e98728706135dc0b636bccdbd65ee278b946e37a
EvaSuper0115 <122745906+EvaSuper0115@users.noreply.github.com>
2023-04-21 17:01:49 +0100

    now "June" is admin and can delete all accounts
    - added "verifyUser" and "verifyAdmin" function to verifyToken.js function

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { verifyToken } from "../utils/verifyToken.js";
+import { verifyToken, verifyUser, verifyAdmin } from "../utils/verifyToken.js";
 import {
   updateUser,
   deleteUser,
@@ -10,6 +10,13 @@ const router = express.Router();
 
 router.get("/checkauthentication", verifyToken, (req, res, next) => {
   res.send("Welcome user, you are logged in.");
+});
+
+router.get("/checkuser/:id", verifyUser, (req, res, next) => {
+  res.send("Welcome user, you are logged in and you can delete your account.");
+});
+router.get("/checkadmin/:id", verifyAdmin, (req, res, next) => {
+  res.send("Welcome admin, you are logged in and you can delete all accounts.");
 });
 //update
 router.put("/:id", updateUser);
